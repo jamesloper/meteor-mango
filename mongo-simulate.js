804e9a54c84179703affe0469e078ca002f988eb
James <james@hux.com>
2018-02-22 14:21:35 -0500

    simplify

--- mongo-simulate.js
+++ mongo-simulate.js
@@ -2,19 +2,17 @@ const {LocalCollection} = require('meteor/minimongo');
 const Test = new LocalCollection(null);
 
 const simulateUpdate = (doc, query, modifier) => {
-	// Check if the modifier contains a positional operator
-	if (Object.keys(modifier).find(k => k.includes('$'))) {
-		Test.insert(doc);
-		Test.update(query, modifier);
-		let res = Test.findOne(doc._id);
-		Test.remove(doc._id);
-		return res;
-	}
+	Test.insert(doc);
+	Test.update(query, modifier);
+	let res = Test.findOne(doc._id);
+	Test.remove(doc._id);
+	return res;
+};
 
-	// Use the more efficient route if there is no positional operator
+const simpleSimulateUpdate = (doc, query, modifier) => {
 	let clonedDoc = EJSON.clone(doc);
 	LocalCollection._modify(doc, modifier);
 	return clonedDoc;
 };
 
-module.exports = {simulateUpdate};
+module.exports = {simulateUpdate, simpleSimulateUpdate};
